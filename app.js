41572fee97150b22be3e4ee3638f9ab9fa934b1a
João Diniz <joaodiniz@msn.com>
2023-05-11 19:01:03 -0300

    installed ejs

--- app.js
+++ app.js
@@ -3,14 +3,40 @@ const app = express()
 
 const port = 3000
 
-app.use(express.urlencoded( {extended: false }))
-app.use(express.json()) // for parsing application/json
+// To take objects from a form
+app.use(express.urlencoded( {extended: true}))
+//app.use(express.json()) // for parsing application/json
 
 app.use(express.static('public'))
+
+// Example
+const myLogger = function (req, res, next) {
+    console.log('LOGGED')
+    next()
+}
+  
+app.use(myLogger)
+////
+
+// Example
+const requesTime = function (req, res, next) {
+    req.requestTime = Date.now()
+    next()
+  }
+  
+  app.use(requesTime)
+///
+
 
 app.get("/", (req, res) => {
     res.send("Home")
 })
+
+app.get('/teste', (req, res) => {
+    let responseText = 'Hello World!<br>'
+    responseText += `<small>Requested at: ${req.requestTime}</small>`
+    res.send(responseText)
+  })
 
 app.get("/user", (req, res) => {
     res.send("User Area")
@@ -24,9 +50,8 @@ app.post("/post/newpost", (req, res) => {
     const post = req.body
     console.log(post)
     res.send("Got your post")
-    
+    //res.json(post)
 })
-
 
 // Server listening
 app.listen(3000, () => {
